386e283bdf51671412cbf7d6d3d09b5514a35838
Marina <maridigiolo@gmail.com>
2017-05-16 09:08:46 -0500

    commitpolls

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(\S+)/answer/$', views.answer, name='answer'),
-    url(r'^(\S+)/vote/(\d+)/$', views.vote, name='vote'),
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^(\S+)/answer/$', views.answer, name='answer'),
+    # url(r'^(\S+)/vote/(\d+)/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,35 @@
-from django.template.response import TemplateResponse
-from django.shortcuts import get_object_or_404
+# from django.template.response import TemplateResponse
+# from django.shortcuts import get_object_or_404
 
 from django.http import HttpResponse, HttpResponseRedirect
 
 from polls.models import Poll, Choice, Category
 
 def index(request):
-    polls = Poll.objects.all()
+    # polls = Poll.objects.all()
+    #
+    # category = request.GET.get('category', '')
+    # if category:
+    #     polls = polls.filter(categories__slug=category)
+    #
+    # context = {
+    #     'polls': polls,
+    #     'selected_category': category
+    # }
+    # return TemplateResponse(request, 'polls.html', context)
+    return HttpResponse("Hello, world. You're at the polls index.")
 
-    category = request.GET.get('category', '')
-    if category:
-        polls = polls.filter(categories__slug=category)
-
-    return TemplateResponse(request, 'polls.html', {'polls': polls})
-
-def answer(request, pollslug):
-    choices = Choice.objects.filter(question__slug=pollslug)
-    return TemplateResponse(request, 'answer.html', {'choices': choices})
-
-def vote (request, pollslug, choice_id):
-    # lookup the choice
-
-    # add a vote
-    # save
-    return HttpResponseRedirect('../../')
+# def detail(request, question_id):
+#     question = get_object_or_404(question__slug, pk=question_id)
+#     return HttpResponse(request, 'polls/detail.html', {'question':question})
+#
+# def answer(request, pollslug):
+#     choices = Choice.objects.filter(question__slug=pollslug)
+#     return TemplateResponse(request, 'answer.html', {'choices': choices})
+#
+# def vote (request, pollslug, choice_id):
+#     # lookup the choice
+#
+#     # add a vote
+#     # save
+#     return HttpResponseRedirect('../../')
